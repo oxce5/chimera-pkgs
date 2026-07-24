bff6b27c4355fe412c4dd3faad9163979e0f56b0
avg.gamer <avg.gamer@proton.me>
2026-07-24 12:11:26 +0800

    flake-parts for flake
    mostly quasigod's code yet again, credits to them

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Chimera packages";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    pkgs-by-name-for-flake-parts.url = "github:drupol/pkgs-by-name-for-flake-parts";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.pkgs-by-name-for-flake-parts.flakeModule ];
+
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+
+      perSystem =
+        { pkgs, system, config, ... }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              (final: prev: {
+                local = config.packages;
+              })
+            ];
+          };
+
+          pkgsDirectory = ./pkgs/by-name;
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [ go gopls ];
+          };
+        };
+
+      flake.nixosModules.sliver =
+        { lib, pkgs, ... }:
+        {
+          imports = [ ./modules/sliver ];
+          services.sliver.package = lib.mkDefault
+            self.packages.${pkgs.stdenv.hostPlatform.system}.sliver;
+        };
+    };
+}
