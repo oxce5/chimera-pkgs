d50a2a34e38f5d405343a163dea808863c493171
avg.gamer <avg.gamer@proton.me>
2026-07-24 12:13:48 +0800

    actually use the right structure

--- flake.nix
+++ flake.nix
@@ -7,38 +7,43 @@
     pkgs-by-name-for-flake-parts.url = "github:drupol/pkgs-by-name-for-flake-parts";
   };
 
-  outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ inputs.pkgs-by-name-for-flake-parts.flakeModule ];
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [inputs.pkgs-by-name-for-flake-parts.flakeModule];
 
       systems = inputs.nixpkgs.lib.systems.flakeExposed;
 
-      perSystem =
-        { pkgs, system, config, ... }:
-        {
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [
-              (final: prev: {
-                local = config.packages;
-              })
-            ];
-          };
-
-          pkgsDirectory = ./pkgs/by-name;
-
-          devShells.default = pkgs.mkShell {
-            packages = with pkgs; [ go gopls ];
-          };
+      perSystem = {
+        pkgs,
+        system,
+        config,
+        ...
+      }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            (final: prev: {
+              local = config.packages;
+            })
+          ];
         };
 
-      flake.nixosModules.sliver =
-        { lib, pkgs, ... }:
-        {
-          imports = [ ./modules/sliver ];
-          services.sliver.package = lib.mkDefault
-            self.packages.${pkgs.stdenv.hostPlatform.system}.sliver;
+        pkgsDirectory = ./pkgs-by-name;
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [go gopls];
         };
+      };
+
+      flake.nixosModules.sliver = {
+        lib,
+        pkgs,
+        ...
+      }: {
+        imports = [./modules/sliver];
+        services.sliver.package =
+          lib.mkDefault
+          self.packages.${pkgs.stdenv.hostPlatform.system}.sliver;
+      };
     };
 }
