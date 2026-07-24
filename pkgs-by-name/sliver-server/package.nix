4650ca5402b62e4b083c4243c9548a3a69ea39ad
avg.gamer <avg.gamer@proton.me>
2026-07-25 00:41:30 +0800

    ai slop patch to fix implant generation

--- pkgs-by-name/sliver-server/package.nix
+++ pkgs-by-name/sliver-server/package.nix
@@ -3,6 +3,9 @@
   stdenv,
   buildGoModule,
   fetchFromGitHub,
+  fetchurl,
+  go,
+  zip,
 }:
 buildGoModule rec {
   pname = "sliver-server";
@@ -17,6 +20,34 @@ buildGoModule rec {
 
   vendorHash = null;
 
+  garble = buildGoModule {
+    pname = "garble";
+    version = "1.26.2";
+    src = fetchFromGitHub {
+      owner = "moloch--";
+      repo = "garble";
+      rev = "v1.26.2";
+      hash = "sha256-AZGGHUvR0eUkdMSgYIco2IhVBvMbzLosek4rcrXmydM=";
+    };
+    vendorHash = "sha256-B8M0SuxiNhq8SEwQGt5qquoYDYyVcr5Mj68vuSPwpcs=";
+    ldflags = [ "-s -w" ];
+    buildPhase = ''
+      runHook preBuild
+      go build -trimpath -ldflags "-s -w" -o garble .
+      runHook postBuild
+    '';
+    installPhase = ''
+      install -Dm755 garble $out/bin/garble
+    '';
+    checkPhase = "";
+    meta.mainProgram = "garble";
+  };
+
+  zigTarXz = fetchurl {
+    url = "https://ziglang.org/download/0.15.2/zig-x86_64-linux-0.15.2.tar.xz";
+    hash = "sha256-AqonDxg9onbltZILHaxEpj8aSeVQUOveOuzJ64L5Mjk=";
+  };
+
   preBuild = let
     os =
       if stdenv.hostPlatform.isLinux
@@ -30,9 +61,23 @@ buildGoModule rec {
       else "amd64";
   in ''
     mkdir -p server/assets/fs/${os}/${arch}
-    printf '\x50\x4b\x05\x06\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' \
-      > server/assets/fs/src.zip
-    touch server/assets/fs/${os}/${arch}/PLACEHOLDER
+
+    cp -r ${go}/share/go $TMPDIR/go
+    chmod -R +w $TMPDIR/go
+
+    rm -rf $TMPDIR/go/api $TMPDIR/go/doc $TMPDIR/go/misc $TMPDIR/go/test
+    rm -f $TMPDIR/go/{AUTHORS,CONTRIBUTORS,PATENTS,VERSION,favicon.ico,robots.txt,SECURITY.md,CONTRIBUTING.md,README.md}
+
+    (cd $TMPDIR/go && ${zip}/bin/zip -r -q $OLDPWD/server/assets/fs/src.zip src/)
+
+    rm -rf $TMPDIR/go/src
+    rm -f $TMPDIR/go/pkg/tool/${os}_${arch}/{doc,tour,test2json}
+
+    (cd $TMPDIR && ${zip}/bin/zip -r -q $OLDPWD/server/assets/fs/${os}/${arch}/go.zip go/)
+
+    cp ${garble}/bin/garble server/assets/fs/${os}/${arch}/garble
+    chmod +x server/assets/fs/${os}/${arch}/garble
+    cp ${zigTarXz} server/assets/fs/${os}/${arch}/zig.tar.xz
   '';
 
   ldflags = [
@@ -40,6 +85,8 @@ buildGoModule rec {
     "-X github.com/bishopfox/sliver/client/command/update.SliverPublicKey=RWTZPg959v3b7tLG7VzKHRB1/QT+d3c71Uzetfa44qAoX5rH7mGoQTTR"
     "-X github.com/bishopfox/sliver/client/assets.DefaultArmoryPublicKey=RWSBpxpRWDrD7Fe+VvRE3c2VEDC2NK80rlNCj+BX0gz44Xw07r6KQD9L"
     "-X github.com/bishopfox/sliver/client/assets.DefaultArmoryRepoURL=https://api.github.com/repos/sliverarmory/armory/releases"
+    "-X github.com/bishopfox/sliver/client/version.Version=${version}"
+    "-X github.com/bishopfox/sliver/client/version.GitCommit=${src.rev}"
     "-X github.com/bishopfox/sliver/server/version.Version=${version}"
     "-X github.com/bishopfox/sliver/server/version.GitCommit=${src.rev}"
     "-X github.com/bishopfox/sliver/server/version.CompiledAt=$(date +%s)"
